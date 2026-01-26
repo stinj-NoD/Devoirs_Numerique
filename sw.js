747495ad8514f71057715a976320a047dba4a2ad
stinj-NoD <stinj.nod@gmail.com>
2026-01-26 21:57:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'devoir-num-v1';
+const CACHE_NAME = 'devoirs-v1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json',
-  './js/app.js',     // Dossier /js/
+  './js/app.js',
   './js/ui.js',
   './js/engines.js',
-  './js/storage.js',
-  './data/index.json', // Dossier /data/
+  './data/french_lib.json',
+  './data/index.json',
+  './manifest.json'
   './data/cp.json',
   './data/ce1.json',
   './data/ce2.json',
@@ -16,17 +16,34 @@ const ASSETS_TO_CACHE = [
   'https://fonts.googleapis.com/css2?family=Quicksand:wght@500;700&display=swap'
 ];
 
-// Installation : Mise en cache des ressources
+// 1. Installation : On met tout en cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Mise en cache des fichiers...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-// Stratégie : Cache d'abord, sinon Réseau
+// 2. Activation : On nettoie les vieux caches si on change de version
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// 3. Interception : On sert le cache si hors ligne
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Si c'est dans le cache, on le rend, sinon on cherche sur internet
       return response || fetch(event.request);
     })
   );
